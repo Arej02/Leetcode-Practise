4f9d8b7f5ad1eafad6ec902d8cc625f952daf12c
Arej02 <159859689+Arej02@users.noreply.github.com>
2024-12-25 16:39:03 +1030

    Leetcode Practise Problem 8(Easy Linked List,Method 1)
    This uses an iterative approach to reverse the Linked List.We can also do this in a recursive way.

---
+++ ReverseofLL1.cpp
@@ -0,0 +1,26 @@
+//This usese an iterative approach to reverse the Linked List.
+#include<iostream>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode*temp=head;
+        ListNode*previous=NULL;
+        
+        while(temp!=NULL){
+            ListNode*front=temp->next;
+            temp->next=previous;
+            previous=temp;
+            temp=front;
+        }
+        return previous; 
+    }
+};
