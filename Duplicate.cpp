8d9e8d688feb6c49dfe6f424313f306c2fefed19
Arej02 <159859689+Arej02@users.noreply.github.com>
2025-01-12 18:13:19 +1030

    Duplicate Element(Medium Array)

---
+++ Duplicate.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> hash(n+1, 0);
+        for(int i=0;i<n;i++){
+            hash[nums[i]]++;
+            if(hash[nums[i]]>1) return nums[i];
+        }
+        return 0;
+        
+    }
+};
